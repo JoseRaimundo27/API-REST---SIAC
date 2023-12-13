b925d03e74274eb20acca013b929367c84212103
José <joseraimundoalves@hotmail.com>
2023-12-13 15:42:56 -0300

    Consumindo minha API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 //ARQUIVO PARA ROTAS 
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const corss = cors();
 
 app.use(express.json()) //Middleware
+app.use(corss)
 
-let password = {
+let dadosLogin = {
     email: "",
     senha: ""
 };
@@ -16,12 +19,12 @@ app.get("/", (req,res) =>{
 
 //Criando rota de cadastro
 app.get("/cadastro", (req,res) => {
-    res.status(200).send(password)
+    res.status(200).send(dadosLogin)
 })
 
 app.post("/cadastro", (req,res) => {
-    password.email = req.body.email
-    password.senha = req.body.senha
-    res.status(201).send([password])
+    dadosLogin.email = req.body.email
+    dadosLogin.senha = req.body.senha
+    res.status(201).send([dadosLogin])
 })
 export default app
