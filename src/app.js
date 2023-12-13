b6028c9581004978af9473b370b45c41df1e8fc2
José PC <joseraimundoalves@hotmail.com>
2023-12-13 10:01:27 -0300

    testando POST e POSTMAN

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 //ARQUIVO PARA ROTAS 
 import express from "express";
 const app = express();
-
+app.use(express.json())
+let password = [{
+    nome: ""
+}];
 //Criando rota padrão
 app.get("/", (req,res) =>{
-    res.send("Olá ")
+    res.status(200).send("Olá ")
 })
 
 //Criando rota de cadastro
 app.get("/cadastro", (req,res) => {
-    res.send("<h1> CADASTRO </h1>")
+    res.status(200).send("<h1> CADASTRO </h1>")
+})
+
+app.post("/cadastro", (req,res) => {
+    password = req.body
+    res.status(201).send(password)
 })
 export default app
